6014a8934a738a2b3e7c2f850ad394bfd7f810c7
santiago jaramillo correa <santijaracor1985@gmail.com>
2025-12-04 23:19:42 -0500

    fix: corregir test compatible con Node 18

--- tests/suma.test.js
+++ tests/suma.test.js
@@ -1,5 +1,6 @@
 import { suma } from "../src/app.js";
-import { test, strict as assert } from "node:test";
+import test from "node:test";
+import assert from "node:assert";
 
 test("La función suma debe sumar correctamente", () => {
   assert.equal(suma(2, 3), 5);
